5ff3bc81a2ef3e2c509ec5927d7e37c1b445e854
moisesBarsoti <moises.barsot2@gmail.com>
2024-10-05 20:07:52 -0300

    Subindo Arquivos

---
+++ Cliente.py
@@ -0,0 +1,80 @@
+import re
+from datetime import datetime
+
+emailRegex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+dataRegex = r'^\d{4}-\d{2}-\d{2}$'
+
+class Cliente:
+    def __init__(self, nomeDoCliente, sobrenomeDoCliente, dataDeAniversario, email, senha): 
+        self.nomeDoCliente = nomeDoCliente
+        self.sobrenomeDoCliente = sobrenomeDoCliente
+        self.dataDeAniversario = datetime.strptime(dataDeAniversario, '%Y-%m-%d')
+        self.email = email
+        self.senha = senha
+        self.saldo = 0  
+
+    def getNome(self):
+        return self.nomeDoCliente
+
+    def getSobrenome(self):
+        return self.sobrenomeDoCliente
+
+    def getDataDeAniversario(self):
+        return self.dataDeAniversario
+
+    def getEmail(self):
+        return self.email
+
+    def getSenha(self):
+        return self.senha
+
+    def getSaldo(self):
+        return self.saldo
+
+    def calcularIdade(self):
+        hoje = datetime.now()
+        idade = hoje.year - self.dataDeAniversario.year - ((hoje.month, hoje.day) < (self.dataDeAniversario.month, self.dataDeAniversario.day))
+        return idade
+    
+    def __str__(self) -> str:
+        return f'{self.nomeDoCliente} {self.sobrenomeDoCliente} {self.dataDeAniversario} {self.email} {self.senha}'
+    
+
+# Recebendo valores
+nome = input('Digite o seu \033[33mnome:\033[m ')
+while nome == '':
+    print(f'\033[31mVocê não pode deixar vazio!\033[m')
+    nome = input('Digite o seu \033[33mnome:\033[m ')
+
+sobrenome = input('Digite o seu \033[33msobrenome:\033[m ')
+while sobrenome == '':
+    print(f'\033[31mVocê não pode deixar vazio!\033[m')
+    sobrenome = input('Digite o seu \033[33msobrenome:\033[m ')
+
+data = input('Digite sua \033[33mdata de aniversário\033[m dessa forma \033[33m(ano-mês-dia):\033[m ')
+while data == '':
+    print(f'\033[31mVocê não pode deixar vazio!\033[m')
+    data = input('Digite sua \033[33mdata de aniversário\033[m dessa forma \033[33m(ano-mês-dia):\033[m ')
+while not re.match(dataRegex, data):
+    print(f'\033[31mVocê tem que colocar dessa forma:\033[m')
+    print(f'\033[36m2000-01-01\033[m')
+    data = input('Digite sua \033[33mdata de aniversário\033[m dessa forma \033[33m(ano-mês-dia):\033[m ')
+
+email = input('Digite o seu \033[33memail:\033[m ')
+while email == '':
+    print(f'\033[31mVocê não pode deixar vazio!\033[m')
+    email = input('Digite o seu \033[33memail:\033[m ')
+while not re.match(emailRegex, email):
+    print(f'\033[31mVocê tem que colocar dessa forma:\033[m')
+    print(f'\033[36mseuEmail@gmail.com\033[m')
+    email = input('Digite o seu \033[33memail:\033[m ')
+
+senha = input('Digite a sua \033[33msenha (digite até 6 números):\033[m ')
+while not senha.isdigit() or len(senha) > 6:
+    print(f'\033[31mVocê não pode deixar vazio e a senha deve ter até 6 números!\033[m')
+    senha = input('Digite a sua \033[33msenha (digite até 6 números):\033[m ')
+
+senhaINT = int(senha)
+
+# Usando valores
+pessoa = Cliente(nome, sobrenome, data, email, senha)
---
+++ funcoes.py
@@ -0,0 +1,96 @@
+from Cliente import pessoa
+
+# Limpar terminal
+import os
+import platform
+
+def limparTerminal():
+    if platform.system() == "Windows":
+        os.system('cls')
+    else:
+        os.system('clear')
+
+
+# Barsoti Bank
+saldo = pessoa.getSaldo()
+extrato = ""
+numeroSaques = 0
+limite = 500
+LIMITE_SAQUES = 3
+
+def opcaoBanco():
+    menu = """
+    [d] Depositar
+    [s] Sacar
+    [v] Ver Saldo
+    [e] Extrato
+    [x] Excluir Conta
+    [q] Sair
+    => """
+    
+    while True:
+        opcao = input(menu)
+        
+        if opcao == "d":
+            depositar()
+        elif opcao == "s":
+            saque()
+        elif opcao == "e":
+            mostrarExtrato()
+        elif opcao == "v":
+            print(f"Seu saldo é: R$ {verSaldo():.2f}")
+        elif opcao == "x":
+            excluirConta()
+        elif opcao == "q":
+            break
+        else:
+            print("Operação inválida, por favor selecione novamente a operação desejada.")
+
+def depositar():
+    global saldo, extrato
+    valor = float(input("Informe o valor do depósito: "))
+    
+    if valor > 0:
+        saldo += valor
+        extrato += f"Depósito: R$ {valor:.2f}\n"
+        print('Operação funcionou! Veja o extrato')
+    else:
+        print("Operação falhou! O valor informado é inválido.")
+
+def saque():
+    global saldo, extrato, numeroSaques
+    valor = float(input("Informe o valor do saque: "))
+    
+    excedeuSaldo = valor > saldo
+    excedeuLimite = valor > limite
+    excedeuSaques = numeroSaques >= LIMITE_SAQUES
+    
+    if excedeuSaldo:
+        print("Operação falhou! Você não tem saldo suficiente.")
+    elif excedeuLimite:
+        print("Operação falhou! O valor do saque excede o limite.")
+    elif excedeuSaques:
+        print("Operação falhou! Número máximo de saques excedido.")
+    elif valor > 0:
+        saldo -= valor
+        extrato += f"Saque: R$ {valor:.2f}\n"
+        numeroSaques += 1
+    else:
+        print("Operação falhou! O valor informado é inválido.")
+        
+def verSaldo():
+    return saldo        
+
+def mostrarExtrato():
+    global extrato, saldo
+    print("\n================ EXTRATO ================")
+    print("Não foram realizadas movimentações." if not extrato else extrato)
+    print(f"\nSaldo: R$ {saldo:.2f}")
+    print("==========================================")
+
+def excluirConta():
+    global saldo, extrato, numeroSaques
+    saldo = 0
+    extrato = ""
+    numeroSaques = 0
+    print("Conta excluída com sucesso.")
